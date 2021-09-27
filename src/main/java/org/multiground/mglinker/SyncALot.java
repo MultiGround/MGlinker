5796d5b13d82fb4e07dd4e5fe9af1dab2ebb0c61
GiftShower <hj.samuel0516@gmail.com>
2021-09-27 21:07:09 +0900

    Fixed Color Handling. Very stupid idea by me.

--- src/main/java/org/multiground/mglinker/SyncALot.java
+++ src/main/java/org/multiground/mglinker/SyncALot.java
@@ -12,6 +12,8 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.Style;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.transformation.TransformationType;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.node.NodeType;
@@ -82,116 +84,16 @@ public class SyncALot{
                     .map(PrefixNode::getMetaValue)
                     .collect(Collectors.toSet());
         }
-        String beforeParse = String.format("[%s]-%s%s > %s", sendAt, prefix.toArray()[0]
+        String toSend = String.format("[%s]-%s%s > %s", sendAt, prefix.toArray()[0]
                 , event.getPlayer().getUsername(), content);
-        //split by color pattern.
-        String[] afterParse = (String[]) COLOR_PATTERN.matcher(beforeParse).results().toArray();
-        Boolean isStyle = false;
-        String writecolor="`";
-        List<String> writeStyle = null;
-        Component result = Component.text("");
-        String[] stylings = {"k", "l", "m", "n", "o", "r"};
-        for (String s : afterParse) {
-            if (!writecolor.equals("`")){
-                Component cmp = Component.text(s).color(TextColor.color(colorParser(writecolor.substring(1))));
-                if(isStyle){
-                    for(String st: writeStyle) {
-                        cmp.style().merge(typeParser(st));
-                    }
-                }
-                result.append(cmp);
-            }
-            if (s.matches(COLOR_PATTERN.pattern())) {
-                for(String s1: stylings){
-                    if(s.endsWith(s1)){
-                        isStyle = true;
-                        if(s.endsWith("r")) {
-                            Objects.requireNonNull(writeStyle).clear();
-                            writecolor = "`";
-                            break;
-                        }
-                        else writeStyle.add(s);
-                        break;
-                    }
-                }
-                if(!isStyle) writecolor = s;
-            }
-        }
+
+        MiniMessage mm = MiniMessage.builder()
+                .transformation(TransformationType.COLOR)
+                .transformation(TransformationType.DECORATION)
+                .transformation(TransformationType.RESET)
+                .build();
         for(Player p: server.getAllPlayers()){
-            p.sendMessage(result);
+            p.sendMessage(mm.parse(toSend));
         }
-    }
-
-
-    private Style typeParser(String styleCode){
-        switch (styleCode) {
-            case "&k" -> {
-                return Style.style(TextDecoration.OBFUSCATED);
-            }
-            case "&l" -> {
-                return Style.style(TextDecoration.BOLD);
-            }
-            case "&m" -> {
-                return Style.style(TextDecoration.STRIKETHROUGH);
-            }
-            case "&n" -> {
-                return Style.style(TextDecoration.UNDERLINED);
-            }
-            case "&o" -> {
-                return Style.style(TextDecoration.ITALIC);
-            }
-            default -> {
-                return Style.empty();
-            }
-        }
-    }
-
-    private TextColor colorParser(String colorCode){
-        if(colorCode == "&0"){
-            return TextColor.color(0, 0, 0);
-        }
-        else if(colorCode == "&1"){
-            return TextColor.color(0, 0, 170);
-        }
-        else if(colorCode == "&2"){
-            return TextColor.color(0, 170, 0);
-        }
-        else if(colorCode == "&3"){
-            return TextColor.color(0, 170, 170);
-        }
-        else if(colorCode == "&4"){
-            return TextColor.color(170, 0, 0);
-        }
-        else if(colorCode == "&5"){
-            return TextColor.color(170, 0, 170);
-        }
-        else if(colorCode == "&6"){
-            return TextColor.color(255, 170, 0);
-        }
-        else if(colorCode == "&7"){
-            return TextColor.color(170, 170, 170);
-        }
-        else if(colorCode == "&8"){
-            return TextColor.color(85, 85, 85);
-        }
-        else if(colorCode == "&9"){
-            return TextColor.color(85, 85, 255);
-        }
-        else if(colorCode == "&a"){
-            return TextColor.color(85, 255, 85);
-        }
-        else if(colorCode == "&b"){
-            return TextColor.color(85, 255, 255);
-        }
-        else if(colorCode == "&c"){
-            return TextColor.color(255, 85, 85);
-        }
-        else if(colorCode =="&d"){
-            return TextColor.color(255, 85, 255);
-        }
-        else if(colorCode == "&e"){
-            return TextColor.color(255,255,85);
-        }
-        else return TextColor.color(255, 255, 255);
     }
 }
